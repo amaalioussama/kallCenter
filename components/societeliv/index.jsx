68fddfade4987c79088f7e1ca319d14c87eaaa9b
amaalioussama <amaaliousama771@gmail.com>
2024-04-20 21:12:40 +0100

    done

--- components/societeliv/index.jsx
+++ components/societeliv/index.jsx
@@ -14,20 +14,20 @@ const Sclivraison = () => {
       <div className="mt-9">
         <h1 id="tit" className="text-center text-5xl font-arabic mt-9 text-firstbule">شركات التوصيل الشريكة</h1>
       </div>
-      <div className="mt-12">
+      <div className=" flex justify-center mt-12">
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
           spaceBetween={50}
-          slidesPerView={1} // Display one slide at a time
-          autoplay={{ delay: 2000 }} // Autoplay with a delay of 2000ms
-          loop={true} // Enable looping
+          slidesPerView={1} 
+          autoplay={{ delay: 2000 }} 
+          loop={true} 
           className="flex justify-between"
         >
-          <SwiperSlide>
-            <Image src={startup} className=" ml-72 h-14 w-32" alt="Startup Logo" id="startup" objectFit="contain" />
+          <SwiperSlide id="swip">
+            <Image src={startup} className=" h-14 w-32" alt="Startup Logo" id="startup" objectFit="contain"  style={{marginLeft : '600px'}} />
           </SwiperSlide>
-          <SwiperSlide>
-            <Image src={amex} className="  ml-72 h-14 w-32" alt="Startup Logo" id="startup" objectFit="contain" />
+          <SwiperSlide  id="swip">
+            <Image src={amex} className="  h-14 w-32" alt="Startup Logo" id="startup" objectFit="contain" style={{marginLeft : '600px'}} />
           </SwiperSlide>
         </Swiper>
       </div>
